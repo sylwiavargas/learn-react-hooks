cd8ad5ddc17d0d1b0585c0611809484a10f8b59f
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-07-23 15:04:01 -0400

    learning hooks

--- src/exercises/01.js
+++ src/exercises/01.js
@@ -4,12 +4,28 @@ import React from 'react'
 
 function Counter() {
   // 🐨 use React.useState here
+  const [count, setCount] = React.useState(0)
   // 🐨 create an increment function that calls the state updater you get from
   //    React.useState to increment the count
+  const increment = () => setCount(count + 1) // or: state.count = state.count +1
   // 🐨 render the count you get from React.useState inside the button and use
   //    your increment function as the onClick handler.
-  return <button />
+  return <button onClick={increment}>{count}</button>
 }
+
+//
+// ### 💯 accept a step and initialCount
+//
+// Make the counter accept props called `step` and `initialCount` and make the
+// counter increment by the given step and start at the given count.
+// 
+// function Counter({initialCount = 0, step = 1}) {
+//   const [count, setCount] = React.useState(initialCount)
+//   const increment = () => {
+//     setCount(currentCount => currentCount + step)}
+//   return <button onClick={increment}>{count}</button>
+// }
+
 
 ////////////////////////////////////////////////////////////////////
 //                                                                //
--- src/exercises/02.js
+++ src/exercises/02.js
@@ -3,14 +3,24 @@ import React from 'react'
 
 // 🐨 Make a custom hook called useCounter that accepts the step and
 // initialCount and returns the count and increment functions
-
-function Counter({step = 1, initialCount = 0}) {
-  // 💣 remove this (or move it to your custom hook)
+function useCounter({step = 1, initialCount = 0} = {}) {
   const [count, setCount] = React.useState(initialCount)
   const increment = () => setCount(c => c + step)
-  // 🐨 Use your custom useCounter hook to get `count` and `increment`
+  return [count, increment]
+}
+
+function Counter({step, initialCount}) {
+  const [count, increment] = useCounter({step, initialCount})
   return <button onClick={increment}>{count}</button>
 }
+
+// function Counter({step = 1, initialCount = 0}) {
+//   // 💣 remove this (or move it to your custom hook)
+//   const [count, setCount] = React.useState(initialCount)
+//   const increment = () => setCount(c => c + step)
+//   // 🐨 Use your custom useCounter hook to get `count` and `increment`
+//   return <button onClick={increment}>{count}</button>
+// }
 
 ////////////////////////////////////////////////////////////////////
 //                                                                //
